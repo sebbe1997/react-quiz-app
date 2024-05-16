f89e9cc6e31cf98ae1126ab0c2ccbbbe862f0e0a
sebastian larsson <sebbe97@users.noreply.github.com>
2024-05-16 14:08:58 +0200

    Updated ReadMe and added a comment in quizpage.js

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Questions from "../Components/Questions";
 import {QuestionService} from "../services/QuestionService.js";
-import { useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 export default function Quispage(){
     const location = useLocation();
@@ -14,7 +13,8 @@ export default function Quispage(){
     maybe use   https://rapidapi.com/jayeshvijay649/api/quiz26
                 https://rapidapi.com/apininjas/api/trivia-by-api-ninjas
                 https://rapidapi.com/lewispour1994/api/trivia-quiz-questions-api/
-                https://rapidapi.com/ExoWatts/api/trivia12 similar json structure. 
+                https://rapidapi.com/ExoWatts/api/trivia12 similar json structure.
+    create a custom hook for data fetching? useFetch? 
   */
    
     useEffect(() =>{
